be08546b691550c262da2154cd999fea4a86a0a4
tom_glb <goodMorning_pro@163.com>
2019-03-10 16:01:40 +0800

    分布式架构->>通过dubbo实现soa架构-实体类

--- e3-manager/e3-manager-pojo/src/main/java/com/el/e3mall/pojo/TbItemParamItem.java
+++ e3-manager/e3-manager-pojo/src/main/java/com/el/e3mall/pojo/TbItemParamItem.java
@@ -1,8 +1,9 @@
 package com.el.e3mall.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TbItemParamItem {
+public class TbItemParamItem implements Serializable{
     private Long id;
 
     private Long itemId;
--- e3-manager/e3-manager-pojo/src/main/java/com/el/e3mall/pojo/TbOrderItem.java
+++ e3-manager/e3-manager-pojo/src/main/java/com/el/e3mall/pojo/TbOrderItem.java
@@ -1,6 +1,8 @@
 package com.el.e3mall.pojo;
 
-public class TbOrderItem {
+import java.io.Serializable;
+
+public class TbOrderItem implements Serializable{
     private String id;
 
     private String itemId;
